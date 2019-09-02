7b4d98ffaa78f7ce6bfe309807014fe9f8d56cb5
YampierFernandez <Yampier.fernandez@upr.edu>
2019-09-02 12:27:33 -0400

    Added second phase.

--- src/Game/Entities/Dynamic/Player.java
+++ src/Game/Entities/Dynamic/Player.java
@@ -12,6 +12,7 @@ import java.util.Random;
 public class Player {
 
     public int lenght;
+    public int speed;
     public boolean justAte;
     private Handler handler;
 
@@ -30,12 +31,13 @@ public class Player {
         direction= "Right";
         justAte = false;
         lenght= 1;
+        speed=5;
 
     }
 
     public void tick(){
         moveCounter++;
-        if(moveCounter>=5) {
+        if(moveCounter>=speed) {
             checkCollisionAndMove();
             moveCounter=0;
         }
@@ -47,6 +49,113 @@ public class Player {
             direction="Left";
         }if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_RIGHT)){
             direction="Right";
+        }if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_P)){
+            speed--;
+        }if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_M)){
+            speed++;
+        }if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_N)){
+
+        	Tail tail = null;
+        	 switch (direction){
+             case "Left":
+                 if( handler.getWorld().body.isEmpty()){
+                     if(this.xCoord!=handler.getWorld().GridWidthHeightPixelCount-1){
+                         tail = new Tail(this.xCoord+1,this.yCoord,handler);
+                     }else{
+                         if(this.yCoord!=0){
+                             tail = new Tail(this.xCoord,this.yCoord-1,handler);
+                         }else{
+                             tail =new Tail(this.xCoord,this.yCoord+1,handler);
+                         }
+                     }
+                 }else{
+                     if(handler.getWorld().body.getLast().x!=handler.getWorld().GridWidthHeightPixelCount-1){
+                         tail=new Tail(handler.getWorld().body.getLast().x+1,this.yCoord,handler);
+                     }else{
+                         if(handler.getWorld().body.getLast().y!=0){
+                             tail=new Tail(handler.getWorld().body.getLast().x,this.yCoord-1,handler);
+                         }else{
+                             tail=new Tail(handler.getWorld().body.getLast().x,this.yCoord+1,handler);
+
+                         }
+                     }
+
+                 }
+                 break;
+             case "Right":
+                 if( handler.getWorld().body.isEmpty()){
+                     if(this.xCoord!=0){
+                         tail=new Tail(this.xCoord-1,this.yCoord,handler);
+                     }else{
+                         if(this.yCoord!=0){
+                             tail=new Tail(this.xCoord,this.yCoord-1,handler);
+                         }else{
+                             tail=new Tail(this.xCoord,this.yCoord+1,handler);
+                         }
+                     }
+                 }else{
+                     if(handler.getWorld().body.getLast().x!=0){
+                         tail=(new Tail(handler.getWorld().body.getLast().x-1,this.yCoord,handler));
+                     }else{
+                         if(handler.getWorld().body.getLast().y!=0){
+                             tail=(new Tail(handler.getWorld().body.getLast().x,this.yCoord-1,handler));
+                         }else{
+                             tail=(new Tail(handler.getWorld().body.getLast().x,this.yCoord+1,handler));
+                         }
+                     }
+
+                 }
+                 break;
+             case "Up":
+                 if( handler.getWorld().body.isEmpty()){
+                     if(this.yCoord!=handler.getWorld().GridWidthHeightPixelCount-1){
+                         tail=(new Tail(this.xCoord,this.yCoord+1,handler));
+                     }else{
+                         if(this.xCoord!=0){
+                             tail=(new Tail(this.xCoord-1,this.yCoord,handler));
+                         }else{
+                             tail=(new Tail(this.xCoord+1,this.yCoord,handler));
+                         }
+                     }
+                 }else{
+                     if(handler.getWorld().body.getLast().y!=handler.getWorld().GridWidthHeightPixelCount-1){
+                         tail=(new Tail(handler.getWorld().body.getLast().x,this.yCoord+1,handler));
+                     }else{
+                         if(handler.getWorld().body.getLast().x!=0){
+                             tail=(new Tail(handler.getWorld().body.getLast().x-1,this.yCoord,handler));
+                         }else{
+                             tail=(new Tail(handler.getWorld().body.getLast().x+1,this.yCoord,handler));
+                         }
+                     }
+
+                 }
+                 break;
+             case "Down":
+                 if( handler.getWorld().body.isEmpty()){
+                     if(this.yCoord!=0){
+                         tail=(new Tail(this.xCoord,this.yCoord-1,handler));
+                     }else{
+                         if(this.xCoord!=0){
+                             tail=(new Tail(this.xCoord-1,this.yCoord,handler));
+                         }else{
+                             tail=(new Tail(this.xCoord+1,this.yCoord,handler));
+                         } System.out.println("Tu biscochito");
+                     }
+                 }else{
+                     if(handler.getWorld().body.getLast().y!=0){
+                         tail=(new Tail(handler.getWorld().body.getLast().x,this.yCoord-1,handler));
+                     }else{
+                         if(handler.getWorld().body.getLast().x!=0){
+                             tail=(new Tail(handler.getWorld().body.getLast().x-1,this.yCoord,handler));
+                         }else{
+                             tail=(new Tail(handler.getWorld().body.getLast().x+1,this.yCoord,handler));
+                         }
+                     }
+
+                 }
+                 break;
+         }
+         handler.getWorld().body.addLast(tail);
         }
 
     }
